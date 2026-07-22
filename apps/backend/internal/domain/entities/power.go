4b16125d9890c1491e2e4b330fa61907c631087f
Oriol Orbea <oriol.orbea@gmail.com>
2026-07-22 21:09:13 +0200

    add NewPower and NewStand constructors with validation for attributes

--- apps/backend/internal/domain/entities/power.go
+++ apps/backend/internal/domain/entities/power.go
@@ -1,11 +1,42 @@
 package entities
 
-import "github.com/oorbea/JojoOnePieceSimulator2/internal/domain/enums"
+import (
+	"errors"
+
+	"github.com/oorbea/JojoOnePieceSimulator2/internal/domain/enums"
+)
 
 type Power struct {
 	name        string
 	description string
 	rarity      enums.PowerRarity
+	skills      []string
+}
+
+func NewPower(
+	name string,
+	description string,
+	rarity enums.PowerRarity,
+	skills []string,
+) (*Power, error) {
+	if name == "" {
+		return nil, errors.New("name is required")
+	}
+	if description == "" {
+		return nil, errors.New("description is required")
+	}
+	if !rarity.IsValid() {
+		return nil, enums.ErrInvalidRarity
+	}
+	if len(skills) < 1 {
+		return nil, errors.New("skills are required")
+	}
+	return &Power{
+		name:        name,
+		description: description,
+		rarity:      rarity,
+		skills:      skills,
+	}, nil
 }
 
 func (p Power) Name() string {
@@ -18,4 +49,8 @@ func (p Power) Description() string {
 
 func (p Power) Rarity() enums.PowerRarity {
 	return p.rarity
+}
+
+func (p Power) Skills() []string {
+	return p.skills
 }
