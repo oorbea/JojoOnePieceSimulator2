3537accf605f18baacf2135de74000a60770fbf8
Oriol Orbea <oriol.orbea@gmail.com>
2026-07-22 20:32:38 +0200

    add Power and PowerRarity types with validation and parsing methods

---
+++ apps/backend/internal/domain/entities/power.go
@@ -0,0 +1,9 @@
+package entities
+
+import "github.com/oorbea/JojoOnePieceSimulator2/internal/domain/enums"
+
+type Power struct {
+	name        string
+	description string
+	rarity      enums.PowerRarity
+}
