65c3342564b2577d8fa0ea50010b8b381b22b324
Oriol Orbea <oriol.orbea@gmail.com>
2026-07-27 19:55:00 +0200

    update router middleware; replace RealIP with ClientIPFromRemoteAddr for improved client IP handling

--- apps/backend/internal/infrastructure/api/endpoints/router.go
+++ apps/backend/internal/infrastructure/api/endpoints/router.go
@@ -14,7 +14,7 @@ func NewRouter(standEndpoints *StandEndpoints) http.Handler {
 	r := chi.NewRouter()
 
 	r.Use(middleware.RequestID)
-	r.Use(middleware.RealIP)
+	r.Use(middleware.ClientIPFromRemoteAddr)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.Timeout(60 * time.Second))
